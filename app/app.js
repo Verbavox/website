852e8e138befd980de959b91de85f4dab366a38a
Hosein Ghasemizade <ghasemizade.hosein@gmail.com>
2024-03-27 19:52:37 +0330

    cahnge app

--- app/app.js
+++ app/app.js
@@ -2,25 +2,30 @@
 import React from 'react'
 
 export default function App() {
-  return (
+//   const [paragraphShow, setParagraphShow] = useState('')
+  
+    return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
         <p>Verbavox</p>
-
-        <button 
-            onClick={async () => {
-                const response = await fetch("/api/chat-gpt", {
-                    method: "POST",
-                    headers: {
-                        "Content-Type": "application/json",
-                    },
-                    body: JSON.stringify({
-                        someData: true,
-                    }),
-                });
-                console.log(response);
-            }}>
+        
+        <button>
             Generate
         </button>
     </main>
   )
 }
+
+// gpt api catch
+
+// onClick={async () => {
+//     const response = await fetch("/api/chat-gpt", {
+//         method: "POST",
+//         headers: {
+//             "Content-Type": "application/json",
+//         },
+//         body: JSON.stringify({
+//             someData: true,
+//         }),
+//     });
+//     console.log(response);
+// }}
